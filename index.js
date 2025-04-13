f697ad879af32c79f302209e3abae8a20a4e541f
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-04-14 00:08:07 +0530

    Fix duplicate mongoose declaration

--- index.js
+++ index.js
@@ -33,11 +33,3 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error(err));
